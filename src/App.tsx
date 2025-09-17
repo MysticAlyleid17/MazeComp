24dd70da801f1cc68a3d33541170049b45f764d9
DarkVelarion17 <176517032+DarkAyleid17@users.noreply.github.com>
2025-09-17 20:22:02 +0200

    wip: HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function HomePage() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <span className="text-[hsl(280,100%,70%)]">Shadows Arcade</span> 
+        </h1>
+        <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:gap-8">
+          <a
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="/maze/index.html"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">Maze →</h3>
+            <div className="text-lg">
+              Play Maze
+            </div>
+          </a>
+          <a
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="/pong/index.html"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">Pong →</h3>
+            <div className="text-lg">
+              Play Pong
+            </div>
+          </a>
+           <a
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="/convey/index.html"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">Conway's Game of Life →</h3>
+            <div className="text-lg">
+              Play Conway's Game of Life
+            </div>
+          </a>
+          <a
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="/tictactoe/index.html"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">Tic Tac Toe →</h3>
+            <div className="text-lg">
+              Play Tic Tac Toe
+            </div>
+          </a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
-
-export default App
